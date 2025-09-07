82fb057924cac3bdd9addc9df4688ec1e167dcd2
ayushs45 <ayush.singh@repello.ai>
2025-09-08 01:24:51 +0530

    Update injection.py

--- injection.py
+++ injection.py
@@ -149,7 +149,7 @@ trainer = Trainer(
     args=training_args,
     train_dataset=train_ds,
     eval_dataset=val_ds,
-    tokenizer=tokenizer,
+    processing_class=tokenizer,
     compute_metrics=compute_metrics,
 )
 
